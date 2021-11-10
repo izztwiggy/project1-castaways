39fcb61a80326e3d50e3b1be5a743c5197123f71
ssallquiz <sallquist.sylvie@gmail.com>
2021-11-10 01:18:20 -0800

    completed Main README.md and single player psuedocode

---
+++ README.md
@@ -0,0 +1,34 @@
+Castaways - A version of Spaceships SEI
+In this project One, we will be building Castaways, a game where a secret word is generated, once generated a coresponding amount of blank spaces to represent the letters in said word. The player then gets to make their play by choosing one letter out of the alphabet. If the letter is correct, it is then displayed in the placeholder word in it's respective spot. If the guess is incorrect, the player then loses a turn and the player loses an "item" and a point. The game is over either once the player guesses the word and still has points/items left, or the player has 0 items/points left and the word has not been correctly guessed. 
+
+For the game, the story is, The player was on a plane that crashed, once they woke up they were stranded on an island with survival items, a boat, and a monster. The monster wants to eat the player, but wants to play a game first. So the monster challenges the player to play castaways, if the player can guess the word via the rules in above paragraph, the player will win, and the moster will let the player get into the boat and sail away. If the player runs out of guesses before guessing the word, then the player will lose and has to stay on the island. The player should have the option to challenge the moster to another game where a game will be restarted, or to exit the game. 
+
+There should be 3 levels of difficulty - the difficulty is the amount of incorrect guesses the user will get. Easy = 10 wrong guesses - 10 points/items
+medium = 8 wrong guesses - 8 points/items
+hard = 6 wrong guesses - 6 points/items
+If the user can guess the word prior to losing all their points then they will be awarded with another life. And the chance to play another game, or escape the island and exit the game.
+
+
+User Stories:
+MVP:
+as a user I want to have a page to enter the game
+as a user I want to clearly see the instructions
+as a user I want to enter my name and have it be displayed on the page
+as a user I want to choose my level of difficulty
+as a user I want to be able to press a button to display placeholders for letters in a word
+as a user I want to know what letter I have guessed 
+as a user I want to know if my guess was correct by having the placeholder replaced by the letter
+as a user I want to know how many remaining guesses I have left
+as a user I want to chose to play another game or leave 
+as a user I want to have a score created and updated for each win that I incur
+as a user I want to be able to see that score if I leave the page then come back later
+as a user I want to see a definition for the word that was generated after the fact if wanted
+as a user I want to choose the monster I see on the screen 
+as a user I want to choose my avatar on the screen
+as a user I want to move through different levels of play 
+as a user I want it clearly and cleanly displayed the amount of points I have left
+as a user I want it clearyly and cleanly displayed the level of difficulty I am on
+as a user I want the option to change my level of diffivulty after each game
+as a user I want to be able to exit to the home page at any point
+as a user I want to have the option to play with a friend 
+as a user I want the option to collect lives and earn points through correct guesses
---
+++ app.js
@@ -0,0 +1,66 @@
+//Step 1
+//starting off, will have a list of 10 words in array and assign it to the variable of wordsList
+//then create a random number generator from 0 to the length of random word array -1
+//then use the random generator to randomly select a word from the array
+//console.log(chosenWord) to give yourself a sanity check 
+
+//have user enter an inputed guess with input form
+//change their answer to lowercaset to match with the wordsList case
+//write simple checker, for letter in chosen random word, if the guess is equal to that iterated letter, return true. Otherwise return false. In this loop return a print true + letter, or false + letter to view 
+
+//step 2 
+//create blank placeholders array for the letters in chosenWord
+//create a display list placeholder
+//loop through the word and for each letter, append a _ to the placeholder array
+//have the user input a guess
+//loop through each position in the chosen word, if the letter matches the guess, the array element is now equal to the guess
+
+//step 3, ask for guesses until the word is populated on the screen 
+//use while loop. While there are _blanks left in the array, continue to prompt the user for a guess
+//keep asking until the player guesses the word (or in later version the player runs out of points)(not to get ahead of myself but that would look like, while (gameOver !== true)) 
+//loop will only stop once display array has no more blanks
+//check if array contains "_", for display.contains('_') // if true keep looping if false, game over = true, if gameOver is true and player has points left over then the player would win. 
+//again not to get ahead of myself but the bones of this games loop will look like:
+/*
+while (gameOver !== true) {
+    guess = input('Enter your guessed Letter').toLocaleLowerCase()
+    //check for the guessed letter in the secretWord
+    for the letter in the secretword
+    can log: the current position of the letter is _, the current letter is _, and your guessed letter is _
+    if letter is equal to guess, dislpayArray[index of letter in for loop] is equal to the guessed letter
+    if no blanks left in array, end of game is true and we exit the loop,
+    otherwise ask user for guess again 
+}
+*/
+//Step 4 - creating player lives and items left
+//create a counter to hold the score, if the guess is not in the chosen secretWord then one point is deducted from the score. If lives is equal to 0, then game over is true
+//create an array to hold images of items, the number of items is equivelent to the number of points left in the game. Will leave the item array empty for now, that will be a later step
+//start with 8 lives, (changing Easy to 8, med to 6, hard to 5)
+
+//step 5 - display the word and spaces on the screen
+//once displayed on the screen, update the array to contain icons of survival items
+//once that is completed, fetch the merriam webster api and update the list of random words to be an Array.from the json data returned.
+//once the api is updated, add the ability to localStorage to keep track of total games played. 
+//once you have accurate working order of number of games played, work through the pages css to aquire it to wireframe status
+//once base css background has been set, set levels of difficulty easy / medium / hard and link to the buttons (actually do this before finishing up css)
+//once levels have been set, add an option to have a timer to guessing period. If you do not guess the word before the time is up, then game is over.
+//easy time length will be 2 min
+//med is 1:15 min
+//hard is 45 sec - 1 min, will research what the best option would be to maximize intrigue and keep players challenged
+
+//step 5, add levels to the story, after 2 - 3 games in one location, give the player the option (if they have won) they can "travel" to the next story location. The next levels should grow in difficulty in one or more aspects, so getting the 5th and final location would be a challenge and desired
+//location one = the island of the crash
+//location 2 = in the sea, a seamonster is trying to attack
+//location 3 = still in the sea, made it past the monster but now there is a horrible storm
+//location 4 = land but chased by ravished animal up the mountain, due to global warming and disgusting corportaion patterns the animals are hungry and losing shelter, so have to deal with a very hungry and grumpy animal
+//location 5 = abducted by aliens and are in space
+
+//step6 introduce double Play 
+//2 players enter their names, have a shuffle to decide who will go first entering word, and who will go first guessing.
+//display the results, take the word choosing player to the "back end " and have them enter a word into a prompt. Check for numbers and characters that are not letters
+//take the players to the guessing stage. 
+//the blanks are updated and displayed for the first players entered word, the guessing player has _ attempts to guess wrong based on the level of difficulty they choose, 
+//have guesser go through game and display and work the game with same logic as above for single player, but at the end, give the guessing player the option to switch places and enter a word, or redo their current spot
+//as a multiplayer user, I want to clearly see whos turn it is to guess, so display the users name on the page, 
+//display the amount of points left 
+//mulitplayers can have option to add timer to guesses
