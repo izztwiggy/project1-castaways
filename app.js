73612b5ca500e09ee9c9b3f5e90e838541c8b524
ssallquiz <sallquist.sylvie@gmail.com>
2021-11-10 19:47:06 -0800

    created buttons for letters A - Z and added sample event listener

--- app.js
+++ app.js
@@ -1,69 +1,63 @@
 //Step 1
 //list of 10 words in array and assign it to the variable of wordsList
-class Levels {
-    constructor(difficulty, points){
-        this.difficulty = difficulty
-        this.points = points
-    }
-}
+// class Levels {
+//     constructor(difficulty, points){
+//         this.difficulty = difficulty
+//         this.points = points
+//     }
+// }
+
+// const wordList = ["migration", "background", "sentiment", "tiger", "bang", "wash", "meaning", "brainstorm", "expectation", "question", "apple"]
+// const placeHolder = []
+//                     //random word picker
+// const randomIndex = Math.floor(Math.random() * wordList.length)
+// let randomWord = wordList[randomIndex]
+// console.log(`The word is ${randomWord}`)
+//                     //create blanks for the word
+// for (let i = 0; i < randomWord.length; i++) {
+//     placeHolder.push("_")
+// }
 
 // let points = 10
-const wordList = ["migration", "background", "sentiment", "tiger", "bang", "wash", "meaning", "brainstorm", "expectation", "question", "apple"]
-const placeHolder = []
-                    //random word picker
-const randomIndex = Math.floor(Math.random() * wordList.length)
-let randomWord = wordList[randomIndex]
-console.log(`The word is ${randomWord}`)
-                    //create blanks for the word
-for (let i = 0; i < randomWord.length; i++) {
-    placeHolder.push("_")
-}
-// let guess = prompt('Enter letter here').toLocaleLowerCase()
-// for (let i = 0; i < randomWord.length; i ++) {
-//     if (randomWord[i] == guess) {
-//         placeHolder[i] = guess
-//     } else {
+// let gameOver = false
+// while (gameOver === false) {
+//     console.log(placeHolder)
+//     console.log(`You have ${points} points`)
+//     let guess = prompt('Enter letter here').toLocaleLowerCase()
+//     if (guess )
+//     if (randomWord.includes(guess) === false) {
 //         points -= 1
-//     } 
+//         if (points <= 0) {
+//             console.log(`You are out of guesses, you lose`)
+//             gameOver = true
+//         }
+//     }
+//     for (let i = 0; i < randomWord.length; i ++) {
+//         if (randomWord[i] === guess) {
+//             placeHolder[i] = guess
+//         }
+//     }
+//     if(placeHolder.includes('_') == false) {
+//         gameOver = true
+//         console.log(`You win`)
+//     }
 // }
-// console.log(placeHolder)
-let points = 10
-let gameOver = false
-while (gameOver === false) {
-    console.log(placeHolder)
-    console.log(`You have ${points} points`)
-    let guess = prompt('Enter letter here').toLocaleLowerCase()
-    if (randomWord.includes(guess) === false) {
-        points -= 1
-        if (points <= 0) {
-            console.log(`You are out of guesses, you lose`)
-            gameOver = true
-        }
-    }
-    for (let i = 0; i < randomWord.length; i ++) {
-        if (randomWord[i] === guess) {
-            placeHolder[i] = guess
-        }
-    }
-    if(placeHolder.includes('_') == false) {
-        gameOver = true
-        console.log(`You win`)
-    }
+
+//loop over the alphabet letters and create buttons
+//A = 65 - Z = 91  , small = a-z 97-123
+
+let letters = document.querySelector('.letters')
+for (let i = 65; i <= 90; i++) {
+    let letter = String.fromCharCode(i)
+    let letterBtn = document.createElement('button')
+    letterBtn.innerText = letter
+    letterBtn.setAttribute('value', letter)
+    letters.append(letterBtn)
+    letterBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        console.log(`I am letter ${letterBtn.value}`)
+    })
 }
-
-
-
-
-
-
-// if (!placeHolder.includes('_')) {
-//     console.log(placeHolder, `You guessed all the letters`)
-// } else {
-//     console.log('guess again')
-// }
-
-
-
 
 
 
