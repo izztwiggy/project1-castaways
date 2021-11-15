ad4b1c1a187af677a3983ead20f573d5413c32cf
ssallquiz <sallquist.sylvie@gmail.com>
2021-11-15 12:35:35 -0800

    working traversal of html buttons between game pages

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const letterDiv = document.querySelector('.letters')
 const displayTemplate = document.querySelector('.displayTemplate')
 const displayPoints = document.querySelector('#pointsSpan')
+const gameInfoDiv = document.querySelector('#gameInfo')
+const levelsSelection = document.querySelector('#levelsModal')
+const openInfo = document.querySelector('#openInfo')
+
 const gameState = {
-    wordList: ["migration", "background", "sentiment", "tiger", "bang", "wash", "meaning", "brainstorm", "expectation", "question", "apple"],
     word: [],
     wordTemplate: [],
     letterGuesses: [],
@@ -11,11 +14,14 @@ const gameState = {
     medium: 7,
     hard: 5,
     points: 0,
-    randomIndex: function(){
-        return Math.floor(Math.random() * this.wordList.length)
-       
+    removeCommas: function(){
+        displayTemplate.append(this.wordTemplate.join(' '))
     },
+    // randomIndex: function(){
+    //     return Math.floor(Math.random() * this.wordList.length)
+    //},
     alphaButtons: function(){
+        //create the alphabet letter Buttons
         for(let i = 65; i <= 90; i ++) {
             const letter = String.fromCharCode(i)
             const letterButtons = document.createElement('button')
@@ -24,20 +30,24 @@ const gameState = {
             letterButtons.innerText = letter
             letterDiv.append(letterButtons)
         }
+        //add the event listener to the letter buttons
         addGlobalEventListener('click', '.letterBtn', (e) => {
             e.preventDefault()
             e.stopPropagation()
-            this.render(this.points, displayPoints)
+            //add the points to the screen
+            //this.render(this.points, displayPoints)     : commented this out as I don;t need this, I am satisfying this in the levels button and the update points function 
+            //guess is the letterBtns value
             const guess = e.target.value
             console.log(guess)
-            //update the guessed List and redisplay on the screen
+            //update the guessed List and redisplay on the screen: will take this out later
             this.letterGuesses.push(guess)
             document.querySelector('#displayGuesses').append(guess)
+            //check the word to see if includes guess
             if (this.word.includes(guess)) {
                 console.log('true')
                 this.updateWordTemplate(guess)
                 if (this.wordTemplate.includes("_") === false) {
-                    console.log('You have won the game')
+                    this.wonGame()
                     //insert you have won the game modal here
                 }
             } else {
@@ -48,37 +58,35 @@ const gameState = {
             e.target.disabled = true
         })
     },
-    //create the blanks on the screen = to characters in word
+    //function to create the blanks on the screen = to characters in word
     addTemplates: function() {
         for(let i = 0; i < this.word.length; i ++) {
             this.wordTemplate.push('_')
         }
     },
-    //create a function to check the guess to the word
+    //function to check position of guess in the word, and updates the wordTemplate
     updateWordTemplate: function(guess) {
         //check which index has the letter and update the word template to reflect
         for (let i = 0; i < this.word.length; i ++) {
             if (this.word[i] === guess) {
                 this.wordTemplate[i] = guess
-                //this works, now need to append the updated wordTemplate to the screen
             }
         }
-        //update the wordTemplate on the screen
-        this.render(this.wordTemplate, displayTemplate)
+        //update the wordTemplate on the screen, needed to do without the commas so instead of render, used empty and commas
+        this.empty(displayTemplate)
+        this.removeCommas()
     },
-    //create function to check to see if points is at 0, if at 0 then will call up the game over screen 
+    //function to check to see if points is at 0, if at 0 then will call up the game over screen 
     checkPoints: function(points) {
         if(points === 0) {
             console.log(`You lost the game`)
             this.empty(displayPoints)
-            //reset the buttons and pop up the You Lost modal
-            // for (button in this.alphaButtons) {
-            //     //need to reset the buttons 
-            // }
+            this.empty(letterDiv)
             // //bring the You lost modal to the screen
+            this.lostGame()
         }
     },
-    //create function to clear out an element - to then replace it
+    //function to clear out an element
     empty: function(element) {
         //grab the children of that element as an array
         let children = Array.prototype.slice.call(element.childNodes)
@@ -92,32 +100,38 @@ const gameState = {
         //empty it out the element, then append the array to said element
         this.empty(element)
         element.append(arr)
+    },
+    lostGame: function(){
+        alert('You lost')
+    },
+    wonGame: function() {
+        alert('You won')
     }
-
-
 }
-
 //create an eListener to the start button to generate the buttons (this will be in the modal pop up)
 addGlobalEventListener('click', '#startBtn', (e) => {
     e.preventDefault()
     const startButton = document.querySelector('#startBtn')
     gameState.alphaButtons()
-    //pick the random word
-    const randomWord = gameState.wordList[gameState.randomIndex()]
-    gameState.word = randomWord.toUpperCase()
-    console.log(randomWord)
-    gameState.addTemplates(randomWord)
-    displayTemplate.append(gameState.wordTemplate)
-    //will add a button to choose difficulty here, so that will be a variable
-    // displayPoints.append(gameState.points)
     //disable the button so can't click and generate more and more alphaButtons. Will need to add on at restart of game 
-    startButton.disabled = true
+        //can take off disbaled as will be on a screen will only access at the start of the game 
+    //startButton.disabled = true
+    gameInfoDiv.style.display = 'none'
     console.log(gameState)
 })
-
-//add listner to the level buttons, will only be able to click on these once, as when you click on these, once mondal is up and working, it will disepear with these buttons, but can come back to this page if needed during game play. Use the buttons to set the remaining guesses. Also 
+//add e listener to the button to open the game info will switch this button out to be the button from the title page
+addGlobalEventListener('click', '#singlePlayBtn', (e) => {
+    e.preventDefault()
+    console.log(e.target)
+    const container = document.querySelector('.container')
+    //container.style.display = 'none'
+    //console.log(gameState.hard)
+}) 
+//add listner to the level buttons, will be contained in a modal, click on the level it will take you back to game play
 addGlobalEventListener('click', '.levelsBtn', (e) => {
     e.preventDefault()
+    levelButton = document.querySelectorAll('.levelsBtn')
+    console.log(levelButton)
     console.log('clicked', e.target.value)
     const level = e.target.value
     for (key in gameState) {
@@ -127,24 +141,31 @@ addGlobalEventListener('click', '.levelsBtn', (e) => {
            displayPoints.append(gameState.points)
         }
     }
+    
     //now fetch the random word api and set the word when you click down here:
-    fetch("https://random-word-api.herokuapp.com/word?number=500&swear=0")
+    fetch("https://random-word-api.herokuapp.com/word?number=1000&swear=0")
         .then(res=> res.json())
         .then(jsonData => {
-            //console.log(jsonData)
+
             const randomNuber = Math.floor(Math.random() * jsonData.length)
-            const randomJson = jsonData[randomNuber]
+            let randomJson = jsonData[randomNuber]
             console.log(randomJson)
-            gameState.word = randomJson
-            console.log(gameState)
+            //check for bad words
+            for (let i = 0; i < blockedWords.length; i ++) {
+                if (randomJson === blockedWords[i] || randomJson.includes(blockedWords[i])) {
+                    randomJson = jsonData[randomNuber]
+                }
+            }
+            gameState.word = randomJson.toUpperCase()
+            gameState.addTemplates(randomJson)
+            gameState.removeCommas()
+            //console.log(jsonData)
         })
         .catch(err => {
             console.log('something went wrong', err)
         })
+        e.target.disabled = true
 })
-
-
-
 
 //create functional event listener on the document to add to specified elements. 
 function addGlobalEventListener(type, selector, callback) {
@@ -154,6 +175,30 @@ function addGlobalEventListener(type, selector, callback) {
         }
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const blockedWords = [
+    'acrotomophilia','alabama hot pocket', 'alaskan pipeline', 'anal','anilingus', 'anus', 'apeshit', 'arsehole', 'ass','asshole', 'assmunch','auto erotic','autoerotic', 'babeland', 'baby batter', 'baby juice','ball gag', 'ball gravy','ball kicking', 'ball licking', 'ball sack', 'ball sucking', 'bangbros', 'bangbus', 'bareback','barely legal', 'barenaked','bastard','bastardo','bastinado','bbw','bdsm', 'beaner', 'beaners', 'beaver cleaver', 'beaver lips','beastiality','bestiality','big black','big breasts','big knockers', 'big tits','bimbos', 'birdlock', 'bitch','bitches', 'black cock', 'blonde action', 'blonde on blonde action',  'blowjob', 'blow job',  'blow your load',  'blue waffle',  'blumpkin',   'bollocks',  'bondage',  'boner',  'boob',   'boobs',  'booty call',  'brown showers',   'brunette action',  'bukkake',   'bulldyke',  'bullet vibe',  'bullshit',   'bung hole', 'bunghole', 'busty',  'butt',  'buttcheeks',  'butthole',  'camel toe',  'camgirl',  'camslut',  'camwhore',  'carpet muncher',  'carpetmuncher',   'chocolate rosebuds',  'cialis',  'circlejerk',  'cleveland steamer', 'clit',  'clitoris',  'clover clamps', 'clusterfuck','cock',   'cocks',  'coprolagnia',  'coprophilia',  'cornhole',  'coon',  'coons',  'creampie',  'cum',  'cumming',  'cumshot',  'cumshots',  'cunnilingus', 'cunt', 'darkie', 'date rape', 'daterape',  'deep throat', 'deepthroat',  'dendrophilia',  'dick', 'dildo', 'dingleberry', 'dingleberries', 'dirty pillows', 'dirty sanchez', 'doggie style',  'doggiestyle',  'doggy style',  'doggystyle',  'dog style', 'dolcett',  'domination',  'dominatrix', 'dommes', 'donkey punch', 'double dong', 'double penetration', 'dp action', 'dry hump',  'dvda', 'eat my ass',  'ecchi',  'ejaculation',  'erotic', 'erotism', 'escort', 'eunuch', 'fag',  'faggot',  'fecal', 'felch',  'fellatio', 'feltch',  'female squirting',  'femdom',  'figging', 'fingerbang',  'fingering',  'fisting',  'foot fetish',  'footjob',  'frotting', 'fuck',  'fuck buttons',  'fuckin',  'fucking', 'fucktards', 'fudge packer',  'fudgepacker',  'futanari', 'gangbang', 'gang bang', 'gay sex',  'genital','giant cock',  'girl on',  'girl on top', 'girls gone wild', 'goatcx',  'goatse',   'god damn', 'gokkun',  'golden shower', 'goodpoop',  'goo girl', 'goregasm',  'grope','group sex', 'g-spot', 'guro',  'hand job', 'handjob',  'hard core',  'hardcore', 'hentai', 'homoerotic','homicide', 'honkey', 'hooker',  'horny', 'hot carl',  'hot chick',  'how to kill',  'how to murder', 'huge fat', 'humping', 'incest',  'intercourse',  'jack off',  'jail bait',   'jailbait', 'jelly donut',  'jerk off',  'jigaboo',  'jiggaboo','jiggerboo',   'jizz',   'juggs',   'kike',   'kinbaku',  'kinkster',  'kinky',  'knobbing', 'leather restraint', 'leather straight jacket', 'lemon party',  'livesex','lolita', 'lovemaking', 'make me come',  'male squirting','masturbate', 'masturbating', 'masturbation','menage a trois','milf', 'missionary position','mong','motherfucker','mound of venus', 'mr hands','muff diver', 'muffdiving', 'nambla', 'nawashi', 'nig','nimphomania','nipple','nipples','nsfw','nsfw images','nude','nudity','nutten','nympho','nymphomania','octopussy', 'omorashi','one cup two girls','one guy one jar','orgasm','orgy','paedophile','paki','panties','panty','pedobear','pedophile','pegging', 'penis', 'phone sex','piece of shit','pikey','pissing','piss pig','pisspig','playboy','pleasure chest','ponyplay','poof','poon','poontang','punany','poop chute','poopchute', 'porn','porno','pornography','prince albert piercing','pthc', 'pubes', 'pussy', 'queaf', 'queef', 'quim','raghead', 'raging boner','rape','raping','rapist', 'rectum', 'reverse cowgirl','rimjob', 'rimming', 'rosy palm', 'rosy palm and her 5 sisters', 'rusty trombone','sadism','santorum','scat','schlong', 'scissoring', 'semen', 'sex', 'sexcam', 'sexo', 'sexy', 'sexual', 'sexually','sexuality','shaved beaver','shaved pussy', 'shemale','shibari', 'shit','shitblimp', 'shitty','shota','shrimping','skeet','slanteye','slut','s&m','smut','snatch','snowballing','sodomize','sodomy','spastic', 'spic','splooge','splooge moose',   'spooge', 'spread legs', 'spunk', 'strap on', 'strapon','strappado',   'strip club', 'style doggy', 'suck',  'sucks', 'suicide girls', 'sultry women',  'swastika','swinger',  'tainted love',  'taste my', 'tea bagging', 'threesome', 'throating', 'thumbzilla', 'tight',  'white',  'tit',  'tits',  'titties','titty', 'tongue', 'topless', 'tosser','towelhead', 'tranny','tribadism', 'tub girl','tubgirl','tushy', 'twat','twink','twinkie','two girls one cup','undressing','upskirt', 'urethra play', 'urophilia','vagina','venus mound', 'viagra', 'vibrator','violet wand', 'vorarephilia','voyeur','voyeurweb','voyuer','vulva','wank','wetback','wet dream','white power','whore','worldsex','wrapping men','wrinkled starfish','xx','xxx','yaoi','yellow showers','yiffy','zoophilia']
 
 
 
